e0414caa994d7b7900e9275bc676db8c62e6c7d7
hani.moafi <hani.moafi74@gmail.com>
2022-06-05 15:56:58 +0430

    swagger

--- launcher/src/main/java/ir/co/isc/assignment/AssignmentApplication.java
+++ launcher/src/main/java/ir/co/isc/assignment/AssignmentApplication.java
@@ -1,17 +1,19 @@
 package ir.co.isc.assignment;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//@Configuration
-//@SpringBootApplication
+
 @EnableAutoConfiguration
 @ComponentScan(basePackages = {"ir.co.isc.*"})
 @EntityScan(basePackages = {"ir.co.isc.entity"})
 @EnableJpaRepositories(basePackages = {"ir.co.isc.repository"})
+@OpenAPIDefinition(info = @Info(title = "account api", version = "1.0", description = "api document for isc assignment"))
 public class AssignmentApplication {
 
     public static void main(String[] args) {
